f37d261ee32be2982c66520de007d29ef77dfb33
titas2001 <tlasic16@student.aau.dk>
2020-10-01 22:31:13 +0300

    Update shamisen maple and matlab

--- shamisen.m
+++ shamisen.m
@@ -145,24 +145,24 @@ mPcr = Ny - floor(Ny/4);   % Plate connection to the bar on the right side y coo
 % String
 uSlMult = (((-2*gammaS^2)/hS^2 - 6*kappaS^2/hS^4 - 4*sigmaS1/(k*hS^2))*k^2 + 2)/(k*sigmaS0 + 1);
 uSl1Mult = (gammaS^2/hS^2 + 4*kappaS^2/hS^4 + 2*sigmaS1/(k*hS^2))*k^2/(k*sigmaS0 + 1);
-uSl2Mult = -1*k^2*kappaS^2/((k*sigmaS0 + 1)*hS^4);
+uSl2Mult = ((-1*k^2*kappaS^2)/(hS^4))/(k*sigmaS0 + 1);
 uSPrevlMult = ((4*sigmaS1*k^2)/(k*hS^2) + k*sigmaS0 - 1)/(k*sigmaS0 + 1);
-uSPrevl1Mult = ((-2*sigmaS1*k^2)*1/(k*hS^2))/(k*sigmaS0 + 1);
+uSPrevl1Mult = ((-2*sigmaS1*k^2)/(k*hS^2))/(k*sigmaS0 + 1);
 
 % Bar
 uBlMult = (2*hB^4 - 4*hB^2*k*sigmaB1 - 6*k^2*kappaB^2)/(hB^4*(k*sigmaB0 + 1));
 uBl1Mult = (2*hB^2*k*sigmaB1 + 4*k^2*kappaB^2)/(hB^4*(k*sigmaB0 + 1));
-uBl2Mult = (-k^2*kappaB^2)/(hB^4*(k*sigmaB0 + 1));
+uBl2Mult = (-1*k^2*kappaB^2)/(hB^4*(k*sigmaB0 + 1));
 uBPrevlMult = (hB^4*k*sigmaB0 - hB^4 + 4*hB^2*k*sigmaB1)/(hB^4*(k*sigmaB0 + 1));
-uBPrevl1Mult = (-2*sigmaB1*k)*1/(hB^2*(k*sigmaB0 + 1));
+uBPrevl1Mult = (-2*sigmaB1*k)/(hB^2*(k*sigmaB0 + 1));
 
 % Plate
 uPlMult = ((-20*kappaPsq/hP^4 - 4*gammaP^2/hP^2 - 8*sigmaP1/(k*hP^2))*k^2 + 2)/(k*sigmaP0 + 1); 
 uPl1Mult = (8*kappaPsq/hP^4 + gammaP^2/hP^2 + 2*sigmaP1/(k*hP^2))*k^2/(k*sigmaP0 + 1);
-uPl1dMult = (-2*kappaPsq*k^2)*1/hP^4/(k*sigmaP0 + 1);
-uPl2Mult = -kappaPsq*k^2/(hP^4*(k*sigmaP0 + 1));
-uPPrevlMult = ((8*sigmaP1*k^2)*1/(k*hP^2) + k*sigmaP0 - 1)/(k*sigmaP0 + 1);
-uPPrevl1Mult = (-2*sigmaP1*k^2)*1/(k*hP^2)/(k*sigmaP0 + 1);
+uPl1dMult = (-2*kappaPsq*k^2)/(hP^4)/(k*sigmaP0 + 1);
+uPl2Mult = (-1*kappaPsq*k^2)/(hP^4)/(k*sigmaP0 + 1);
+uPPrevlMult = ((8*sigmaP1*k^2)/(k*hP^2) + k*sigmaP0 - 1)/(k*sigmaP0 + 1);
+uPPrevl1Mult = ((-2*sigmaP1*k^2)/(k*hP^2))/(k*sigmaP0 + 1);
 
 % Forces
 FsbMult = 1/(1/(rhoB*AreaB*hB) + 1/(rhoS*AreaS*hS));
@@ -171,7 +171,7 @@ FbpMult = 1/(-1/(rhoB*AreaB*hB) - 1/(rhoP*HP*hP^2));
 %%
 tic
 for n = 1:dur
-        % calculate virtual grid points
+    % calculate virtual grid points
     uB0 = 2*uB(1)-uB(2);                    % uB(0)
     uBm1 = 2*(uB0-uB(2))+uB(3);             % uB(-1)
     uBPrev0 = 2*uBPrev(1)-uBPrev(2);        % uBPrev(0)
@@ -186,18 +186,18 @@ for n = 1:dur
 %         ((gammaS^2 * (uS(lS1c) - uS(lS1c+1)/2 - uS(lS1c-1)/2)*k)/2 + sigmaS1*(uS(lS1c) - uS(lS1c+1)/2 - uS(lS1c-1)/2 - ...
 %         uSPrev(lS1c) + uSPrev(lS1c+1)/2 + uSPrev(lS1c-1)/2))*k*hS^2 - (3* (uS(lS1c) - 2*uS(lS1c+1)/3 + uS(lS1c+2)/6 - ...
 %         2*uS(lS1c-1)/3 + uS(lS1c-2)/6)*kappaS^2 * k^2)/2));
-    % Force from strings to the bridge
-    Fsb1 = FsbMult * (uB(lBc1)*uBlMult + (uB(lBc1-1) + uB(lBc1+1))*uBl1Mult + (uB(lBc1-2) + uB(lBc1+2))*uBl2Mult + ...
+    % Force from the Strings to the bridge
+    Fs1b = FsbMult * (uB(lBc1)*uBlMult + (uB(lBc1-1) + uB(lBc1+1))*uBl1Mult + (uB(lBc1-2) + uB(lBc1+2))*uBl2Mult + ...
         uBPrev(lBc1)*uBPrevlMult + (uBPrev(lBc1-1)+ uBPrev(lBc1+1))*uBPrevl1Mult + ...
         uS1(lS1c)*uSlMult + (uS1(lS1c-1) + uS1(lS1c+1))*uSl1Mult + (uS1(lS1c-2) + uS1(lS1c+2))*uSl2Mult + ...
         uS1Prev(lS1c)*uSPrevlMult + (uS1Prev(lS1c-1)+ uS1Prev(lS1c+1))*uSPrevl1Mult);
     
-    Fsb2 = FsbMult * (uB(lBc2)*uBlMult + (uB(lBc2-1) + uB(lBc2+1))*uBl1Mult + (uB(lBc2-2) + uB(lBc2+2))*uBl2Mult + ...
+    Fs2b = FsbMult * (uB(lBc2)*uBlMult + (uB(lBc2-1) + uB(lBc2+1))*uBl1Mult + (uB(lBc2-2) + uB(lBc2+2))*uBl2Mult + ...
         uBPrev(lBc2)*uBPrevlMult + (uBPrev(lBc2-1)+ uBPrev(lBc2+1))*uBPrevl1Mult + ...
         uS2(lS2c)*uSlMult + (uS2(lS2c-1) + uS2(lS2c+1))*uSl1Mult + (uS2(lS2c-2) + uS2(lS2c+2))*uSl2Mult + ...
         uS2Prev(lS2c)*uSPrevlMult + (uS2Prev(lS2c-1)+ uS2Prev(lS2c+1))*uSPrevl1Mult);
     
-    Fsb3 = FsbMult * (uB(lBc3)*uBlMult + (uB(lBc3-1) + uB(lBc3+1))*uBl1Mult + (uB(lBc3-2) + uB(lBc3+2))*uBl2Mult + ...
+    Fs3b = FsbMult * (uB(lBc3)*uBlMult + (uB(lBc3-1) + uB(lBc3+1))*uBl1Mult + (uB(lBc3-2) + uB(lBc3+2))*uBl2Mult + ...
         uBPrev(lBc3)*uBPrevlMult + (uBPrev(lBc3-1)+ uBPrev(lBc3+1))*uBPrevl1Mult + ...
         uS3(lS3c)*uSlMult + (uS3(lS3c-1) + uS3(lS3c+1))*uSl1Mult + (uS3(lS3c-2) + uS3(lS3c+2))*uSl2Mult + ...
         uS3Prev(lS3c)*uSPrevlMult + (uS3Prev(lS3c-1)+ uS3Prev(lS3c+1))*uSPrevl1Mult);
@@ -216,7 +216,7 @@ for n = 1:dur
     
     % Force from bridge' left and right mounting points to the plate
     Fbpl = FbpMult*(uB(lBcl)*uBlMult + (uB0 + uB(lBcl+1))*uBl1Mult + (uBm1 + uB(lBcl+2))*uBl2Mult + ...
-        uBPrev(lBcl)*uBPrevlMult + (uBPrev0+ uBPrev(lBcl+1))*uBPrevl1Mult + ...
+        uBPrev(lBcl)*uBPrevlMult + (uBPrev0 + uBPrev(lBcl+1))*uBPrevl1Mult + ...
         uP(lPcl,mPcl)*uPlMult + (uP(lPcl-1,mPcl) + uP(lPcl+1,mPcl) + uP(lPcl,mPcl+1) + uP(lPcl,mPcl-1))*uPl1Mult + ...
         (uP(lPcl-1,mPcl-1) + uP(lPcl+1,mPcl-1) + uP(lPcl-1,mPcl+1) + uP(lPcl+1,mPcl+1))*uPl1dMult + ...
         (uP(lPcl-2,mPcl) + uP(lPcl+2,mPcl) + uP(lPcl,mPcl-2) + uP(lPcl,mPcl+2))*uPl2Mult + ...
@@ -233,7 +233,7 @@ for n = 1:dur
     
     % UPDATE EQUATIONS
     
-    % Update equation for the Strings
+    %% Update equation for the Strings
 %     uSNext(lS) = (1/(sigmaS0*k + 1)) * (((gammaS^2 * (uS(lS+1) - 2*uS(lS) + uS(lS-1))/hS^2) - ...
 %         (kappaS^2 * (uS(lS+2) - 4*uS(lS+1) + 6*uS(lS) - 4*uS(lS-1) + uS(lS-2))/hS^4) + ...
 %         (2*sigmaS1 * (uS(lS+1) - 2*uS(lS) + uS(lS-1) - uSPrev(lS+1) + 2*uSPrev(lS) - uSPrev(lS-1))/k*hS^2))*k^2 + ...
@@ -245,9 +245,9 @@ for n = 1:dur
     uS3Next(lS) = uS3(lS)*uSlMult + (uS3(lS-1) + uS3(lS+1))*uSl1Mult + (uS3(lS-2) + uS3(lS+2))*uSl2Mult + ...
         uS3Prev(lS)*uSPrevlMult + (uS3Prev(lS-1)+ uS3Prev(lS+1))*uSPrevl1Mult;
     % Update Strings equation at the localizer point with Fsm (Force loss to the bridge)
-    uS1Next(lS1c) = uS1Next(lS1c) - Fsb1/(rhoS * AreaS *hS);
-    uS2Next(lS2c) = uS2Next(lS2c) - Fsb2/(rhoS * AreaS *hS);
-    uS3Next(lS3c) = uS3Next(lS3c) - Fsb3/(rhoS * AreaS *hS);
+    uS1Next(lS1c) = uS1Next(lS1c) - Fs1b/(rhoS * AreaS *hS);
+    uS2Next(lS2c) = uS2Next(lS2c) - Fs2b/(rhoS * AreaS *hS);
+    uS3Next(lS3c) = uS3Next(lS3c) - Fs3b/(rhoS * AreaS *hS);
     
     % Update equation of the Mass spring (bridge)
 %     uMNext(lM) = -4*pi^2*f0^2*k^2*uM(lM) - R*k*(uM(lM) - uMPrev(lM)) + 2*uM(lM) - uMPrev(lM);
@@ -269,18 +269,18 @@ for n = 1:dur
     uBNext(NB) = uB(NB)*uBlMult + (uB(NB-1) + uBNp1)*uBl1Mult + (uB(NB-2) + uBNp2)*uBl2Mult + ...
         uBPrev(NB)*uBPrevlMult + (uBPrev(NB-1) + uBPrevNp1)*uBPrevl1Mult;
     % Update Bar function at the localizer point with Fs1b (Force gain from the string 1)
-    uBNext(lBc1) = uBNext(lBc1) + Fsb1/(rhoB * AreaB *hB);
+    uBNext(lBc1) = uBNext(lBc1) + Fs1b/(rhoB * AreaB *hB);
     % Update Bar function at the localizer point with Fs2b (Force gain from the string 2)
-    uBNext(lBc2) = uBNext(lBc2) + Fsb2/(rhoB * AreaB *hB);
+    uBNext(lBc2) = uBNext(lBc2) + Fs2b/(rhoB * AreaB *hB);
     % Update Bar function at the localizer point with Fs3b (Force gain from the string 3)
-    uBNext(lBc3) = uBNext(lBc3) + Fsb3/(rhoB * AreaB *hB);
+    uBNext(lBc3) = uBNext(lBc3) + Fs3b/(rhoB * AreaB *hB);
     % Update Bar function at the localizer point lBl with Fbp (Force gain from the string 1)
     uBNext(lBcl) = uBNext(lBcl) - Fbpl/(rhoB * AreaB *hB);
     % Update Bar function at the localizer point lBr with Fbp (Force gain from the string 1)
     uBNext(lBcr) = uBNext(lBcr) - Fbpr/(rhoB * AreaB *hB);
 
     
-    %% Update function of the Plate
+    %% Update equation of the Plate
 %     uPNext(lP,mP) = (1/(k*sigmaP0 + 1))*(((-(kappaPsq)/hP^4)*((uP(lP+2,mP) + uP(lP-2,mP) + uP(lP,mP+2) + uP(lP,mP-2)) + ...
 %         2*(uP(lP+1,mP+1) + uP(lP+1,mP-1) + uP(lP-1,mP+1) + uP(lP-1,mP-1)) - ...
 %         8*(uP(lP+1,mP) + uP(lP-1,mP) + uP(lP,mP+1) + uP(lP,mP-1)) + 20*uP(lP,mP)) + ...
