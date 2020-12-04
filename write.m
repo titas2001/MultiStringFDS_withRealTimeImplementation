73234e49f8ba94b2e0ac0c9598c662468abae284
Titas <tlasic16@student.aau.dk>
2020-12-04 02:10:38 +0200

    Update write.m

--- write.m
+++ write.m
@@ -1,4 +1,4 @@
-function write(note,shamisenString, out,Nx,Ny)
+function write(shamisenString, out,Nx,Ny,note)
 M = max(abs(out));
 outN = out/M;
 filename = strcat("C:\University\SMC\SMC10\multiString\MultiStringFDS_withRealTimeImplementation\audio\samples\","string",string(shamisenString),note,"_Grid",string(Nx),"x", string(Ny),".wav");
