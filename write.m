013489b6046658bb19c675b48822d9fc8b82761e
Titas <tlasic16@student.aau.dk>
2020-12-17 03:07:03 +0200

    Update write.m

--- write.m
+++ write.m
@@ -1,7 +1,7 @@
-function write(shamisenString, out,Nx,Ny,note)
+function write(shamisenString, out,Nx,Ny,note,i)
 M = max(abs(out));
 outN = out/M;
-filename = strcat("C:\University\SMC\SMC10\multiString\MultiStringFDS_withRealTimeImplementation\audio\samples\","string",string(shamisenString),note,"_Grid",string(Nx),"x", string(Ny),".wav");
+filename = strcat("C:\University\SMC\SMC10\multiString\MultiStringFDS_withRealTimeImplementation\audio\samples\",string(i),"#","string",string(shamisenString),note,"_Grid",string(Nx),"x", string(Ny),".wav");
 audiowrite(filename, outN, 44100);
 end
 
